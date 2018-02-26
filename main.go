e29180f5d9e931c712efe7a20dbc454b0f6ef6c1
Markus Radspieler <markus@radspieler.org>
2018-02-26 20:47:48 +0100

    added a concurrency fix

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -11,7 +12,10 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"runtime/pprof"
+	"runtime/trace"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/spf13/viper"
@@ -41,7 +45,7 @@ var (
 	userName     string
 	password     string
 	hostName     string
-	foundLinks   uint32
+	workCounter  int32
 )
 
 func initEnv() {
@@ -56,9 +60,23 @@ func initEnv() {
 }
 
 func main() {
+	c, err := os.Create("cpu.pprof")
+	checkerr(err)
+	t, err := os.Create("trace.log")
+	checkerr(err)
+
+	pprof.StartCPUProfile(c)
+	defer pprof.StopCPUProfile()
+
+	trace.Start(t)
+	defer trace.Stop()
+
 	startTime := time.Now().UnixNano()
 
 	initEnv()
+
+	concurrencyPtr := flag.Int("t", 1, "how many threads startet")
+	debugPtr := flag.Bool("d", false, "write debug files")
 
 	flag.Parse()
 	args := flag.Args()
@@ -71,7 +89,18 @@ func main() {
 	checkerr(err)
 	defer f.Close()
 
+	var finvalid, finsert *os.File
+
+	if *debugPtr {
+		finvalid, err = os.Create("invalid_link.log")
+		checkerr(err)
+
+		finsert, err = os.Create("insert_link.log")
+		checkerr(err)
+	}
+
 	ds := createData(f)
+	fmt.Printf("Entries: %d\n", len(ds))
 
 	clientID = viper.GetString("settings.clientID")
 	clientSecret = viper.GetString("settings.clientSecret")
@@ -94,18 +123,16 @@ func main() {
 	byteData, err := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(byteData, &rd)
 
-	concurrency := 2
-	sem := make(chan bool, concurrency)
+	fmt.Println("========================================================")
+
+	sem := make(chan bool, *concurrencyPtr)
 
 	for _, s := range ds[1:] {
 		sem <- true
 
-		go checkURL(sem, s, rd.AccessToken)
+		go checkURL(sem, s, rd.AccessToken, finvalid, finsert)
 
 		if ((time.Now().UnixNano() - accessTokTime) / 1000000000.0) > 3500 {
-			for i := 0; i < cap(sem); i++ {
-				sem <- true
-			}
 			resp, err := http.PostForm(hostName+"/oauth/v2/token", url.Values{"grant_type": {"refresh_token"}, "client_id": {clientID}, "client_secret": {clientSecret}, "refresh_token": {rd.RefreshToken}})
 			if err != nil {
 				log.Fatalf("Error: %#v\n", err)
@@ -115,53 +142,101 @@ func main() {
 			byteData, err := ioutil.ReadAll(resp.Body)
 			json.Unmarshal(byteData, &rd)
 		}
+
 	}
 	for i := 0; i < cap(sem); i++ {
 		sem <- true
 	}
 
-	fmt.Printf("Links OK: %d\n", foundLinks)
+	fmt.Printf("Links OK: %d\n", workCounter)
 	fmt.Printf("\nDuration: %#.4v min.\n", float64((time.Now().UnixNano()-startTime)/1000000000.0)/60)
 }
 
-func checkURL(s chan bool, u diigoCsv, token string) {
-	defer func() { <-s }()
+func retry(c http.Client, dc *diigoCsv) (resp *http.Response, err error) {
+	for i := 0; i < 3; i++ {
+		resp, err = c.Get(dc.url)
+		if err == nil && resp.StatusCode == 200 {
+			return resp, err
+		}
+		time.Sleep(1 * time.Microsecond)
+	}
 
-	resp, err := http.Get(u.url)
+	ur, err := url.Parse(dc.url)
 	if err != nil {
-		// log.Printf("Warning: %#v, %#v\n", u.url, err)
-	} else if resp.StatusCode == 200 {
-		foundLinks++
-		// fmt.Printf("Info: %#v\n", u)
-		form := url.Values{}
-		form.Add("url", u.url)
-		form.Add("title", u.title)
-		if u.tags != "no_tags" {
-			form.Add("tags", u.tags)
-		}
-		form.Add("content", u.description)
-		form.Add("published_at", u.createdAt)
+		log.Fatalf("Parse Error: %s\n", err)
+	}
 
-		req, err := http.NewRequest("POST", hostName+"/api/entries.json", strings.NewReader(form.Encode()))
-		if err != nil {
-			log.Fatalf("Error: %#v\n", err)
+	for i := 0; i < 3; i++ {
+		resp, err = c.Get(ur.Scheme + "://" + ur.Hostname())
+		if err == nil && resp.StatusCode == 200 {
+			dc.url = ur.Scheme + "://" + ur.Hostname()
+			dc.title = ur.Hostname()
+			dc.tags = ""
+			dc.description = ""
+			dc.comments = ""
+			dc.annotations = ""
+			return resp, err
 		}
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Set("Authorization", "Bearer "+token)
+		time.Sleep(1 * time.Microsecond)
+	}
 
-		c := &http.Client{}
+	return nil, errors.New("Could not connect to: " + dc.url + ", " + ur.Scheme + "://" + ur.Hostname())
+}
 
-		resp, err = c.Do(req)
-		if err != nil {
-			log.Fatalf("Post Error: %#v - %#v\n", u, err)
-		}
+func checkURL(sem chan bool, u diigoCsv, token string, finvalid *os.File, finsert *os.File) {
+	defer func() { <-sem }()
+
+	getClient := http.Client{
+		Timeout: time.Duration(3 * time.Minute),
+	}
+
+	resp, err := getClient.Get(u.url)
+	if err != nil || resp.StatusCode != 200 {
+		resp, err = retry(getClient, &u)
+	}
+
+	if err != nil || resp.StatusCode != 200 {
+		finvalid.WriteString(fmt.Sprintf("Error: %s\n", err))
+		return
+	}
+
+	atomic.AddInt32(&workCounter, 1)
+	fmt.Printf("LINK: %d\n", workCounter)
+
+	form := url.Values{}
+	form.Add("url", u.url)
+	form.Add("title", u.title)
+	if u.tags != "no_tags" {
+		form.Add("tags", u.tags)
+	}
+	form.Add("content", u.description)
+	form.Add("published_at", u.createdAt)
+
+	req, err := http.NewRequest("POST", hostName+"/api/entries.json", strings.NewReader(form.Encode()))
+	if err != nil {
+		log.Fatalf("Error: %#v\n", err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	cPost := http.Client{
+		Timeout: time.Duration(10 * time.Minute),
+	}
+
+	resp, err = cPost.Do(req)
+	if err != nil {
+		finsert.WriteString(fmt.Sprintf("Post Error: %s - %s\n", u, err))
+		// log.Fatalf("Post Error: %s - %s\n", u, err)
+	}
+
+	if resp != nil {
 		defer resp.Body.Close()
 
 		if resp.StatusCode != 200 {
-			log.Printf("Info: %#v - %#v\n", resp.StatusCode, u.url)
+			finsert.WriteString(fmt.Sprintf("Warning: %d - %s\n", resp.StatusCode, u.url))
 		}
 	} else {
-		// log.Printf("Warning: %#v - %#v\n", resp.StatusCode, u.url)
+		finsert.WriteString(fmt.Sprintf("Warning: No Response - %s\n", u.url))
 	}
 }
 
